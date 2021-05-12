faeb80e12189f74dd0134348286848caea64dd10
jamalludin eka prasetyo <jamalludin.eka.prasetyo@gmail.com>
2021-05-12 07:03:27 +0700

    menambahkan footer dan index untuk halaman admin

---
+++ admin/index.php
@@ -0,0 +1,15 @@
+<?php
+include 'header.php';
+?>
+<div class="col-md-10">
+    <h3>Selamat datang</h3>
+    <h3>Aplikasi Penjualan Sederhana</h3>
+    <h3>WWW.MALASNGODING.COM</h3>
+</div>
+<!-- kalender -->
+<div class="pull-right">
+    <div id="kalender"></div>
+</div>
+<?php
+require_once "footer.php";
+?>
